e32e271e808e96be3945635f5912d9c308ad8fe7
vanhieuhp <vanhieuit10@gmail.com>
2025-05-26 16:42:52 +0700

    feat: openfeign snippet code
    Signed-off-by: vanhieuhp <vanhieuit10@gmail.com>

---
+++ openfeign/src/test/java/hieunv/dev/openfeign/OpenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package hieunv.dev.openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
